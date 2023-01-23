4bb42fe0aeec3fe711cb8da329046ba03d9cd235
nhh2 <nhernand@chicagobooth.edu>
2023-01-23 05:02:47 +0000

    Payments Halfway

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,33 +5,48 @@ class ApplicationController < ActionController::Base
     render ({ :template => "calculation_templates/square_form.html.erb"})
   end
 
-# Defining the method "blank_square_form"
-  def blank_square_form
-    render ({ :template => "calculation_templates/square_form.html.erb"})
-  end
+## Squares 
+  # Defining the method "blank_square_form"
+    def blank_square_form
+      render ({ :template => "calculation_templates/square_form.html.erb"})
+    end
+  # Defining the method "calculate_square"
+    def calculate_square
+      @num = params.fetch("square_of").to_f
+      @square_of_num = @num ** 2
+      render ({ :template => "calculation_templates/square_results.html.erb"})
+    end
 
+## Square Roots
+  # Defining the method "blank_square_root_form"
+    def blank_square_root_form
+      render ({ :template => "calculation_templates/square_root_form.html.erb"})
+    end
+  # Defining the method "calculate_square_root"
+    def calculate_square_root
+      @rooter = params.fetch("root_of").to_f
+      @root_of_rooter = @rooter ** 0.5
+      render ({ :template => "calculation_templates/square_root_results.html.erb"})
+    end
 
-# Defining the method "calculate_square"
-  def calculate_square
-    
-    @num = params.fetch("square_of").to_f
-    @square_of_num = @num ** 2
+### Payments
+  # Defining the method "blank_square_root_form"
+    def blank_payment_form
+      render ({ :template => "calculation_templates/payment_form.html.erb"})
+    end
+  # Defining the method "calculate_payment"
+    def calculate_payment
+      @annual = params.fetch("apr_of").to_f
+      @timing = params.fetch("horizon").to_f
+      @prin = params.fetch("pmts").to_f
 
-    render ({ :template => "calculation_templates/square_results.html.erb"})
-  end
-
-# Defining the method "blank_square_root_form"
-  def blank_square_root_form
-    render ({ :template => "calculation_templates/square_root_form.html.erb"})
-  end
-
-  def calculate_square_root
-
-    @rooter = params.fetch("root_of").to_f
-    @root_of_rooter = @rooter ** 0.5
-
-    render ({ :template => "calculation_templates/square_root_results.html.erb"})
-  end
-
+      @numerator_base = @annual ** (1 + @annual)
+      @numerator_raised = @numerator_base ** (@timing % 12)
+      @denominator = @numerator_raised - 1
+      
+      @fraction = @numerator_raised % @denominator
+      @final = @prin ** @fraction
+      render ({ :template => "calculation_templates/payment_results.html.erb"})
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,14 @@ Rails.application.routes.draw do
   # Define the /square_root/new URL
     get("/square_root/new", { :controller=> "application", :action => "blank_square_root_form"})
 
-  # Define the /square_root/new URL
+  # Define the /square_root/results URL
     get("/square_root/results", { :controller=> "application", :action => "calculate_square_root"})
+
+## Payment
+  # Define the /payment/new URL
+    get("/payment/new", { :controller=> "application", :action => "blank_payment_form"})
+
+  # Define the /payment/results URL
+    get("/payment/results", { :controller=> "application", :action => "calculate_payment"})
 
 end
