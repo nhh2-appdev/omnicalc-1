6269e82aebc62932ea6b394a6ca448da01e495fd
nhh2 <nhernand@chicagobooth.edu>
2023-01-23 04:38:34 +0000

    Square root working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,37 @@
 class ApplicationController < ActionController::Base
 
+# Defining the method "homepage"
+  def homepage
+    render ({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
 # Defining the method "blank_square_form"
   def blank_square_form
     render ({ :template => "calculation_templates/square_form.html.erb"})
   end
 
+
 # Defining the method "calculate_square"
   def calculate_square
+    
+    @num = params.fetch("square_of").to_f
+    @square_of_num = @num ** 2
+
     render ({ :template => "calculation_templates/square_results.html.erb"})
   end
+
+# Defining the method "blank_square_root_form"
+  def blank_square_root_form
+    render ({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+
+    @rooter = params.fetch("root_of").to_f
+    @root_of_rooter = @rooter ** 0.5
+
+    render ({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
 # Define the /square/new URL
-  get("/square/new", { :controller=> "application", :action => "blank_square_form"})
+  get("/", { :controller=> "application", :action => "homepage"})
 
-# Define the /square/results URL
-  get("/square/results", { :controller=> "application", :action => "calculate_square"})
+## Square
+  # Define the /square/new URL
+    get("/square/new", { :controller=> "application", :action => "blank_square_form"})
+
+  # Define the /square/results URL
+    get("/square/results", { :controller=> "application", :action => "calculate_square"})
+
+## Square Root
+  # Define the /square_root/new URL
+    get("/square_root/new", { :controller=> "application", :action => "blank_square_root_form"})
+
+  # Define the /square_root/new URL
+    get("/square_root/results", { :controller=> "application", :action => "calculate_square_root"})
 
 end
