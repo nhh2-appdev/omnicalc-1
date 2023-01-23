5420942b95c15d7a01473889f67847c4294d8942
nhh2 <nhernand@chicagobooth.edu>
2023-01-23 03:52:06 +0000

    Version 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 
+# Defining the method "blank_square_form"
+  def blank_square_form
+    render ({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+# Defining the method "calculate_square"
+  def calculate_square
+    render ({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+# Define the /square/new URL
+  get("/square/new", { :controller=> "application", :action => "blank_square_form"})
+
+# Define the /square/results URL
+  get("/square/results", { :controller=> "application", :action => "calculate_square"})
+
 end
